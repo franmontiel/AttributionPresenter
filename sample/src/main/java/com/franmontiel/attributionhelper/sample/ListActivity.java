ec4d131f73823f9f568cc6a059b5616fb9963c51
fjmontiel <fjmontiel@spiralapps.com>
2017-06-27 02:33:57 +0200

    Update sample app

---
+++ sample/src/main/java/com/franmontiel/attributionhelper/sample/ListActivity.java
@@ -0,0 +1,21 @@
+package com.franmontiel.attributionhelper.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.franmontiel.attributionhelper.sample.util.AttributionHelperCreator;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_list);
+
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(AttributionHelperCreator.create(this).getAdapter());
+    }
+}
--- sample/src/main/java/com/franmontiel/attributionhelper/sample/MainActivity.java
+++ sample/src/main/java/com/franmontiel/attributionhelper/sample/MainActivity.java
@@ -1,7 +1,17 @@
 package com.franmontiel.attributionhelper.sample;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.franmontiel.attributionhelper.AttributionHelper;
+import com.franmontiel.attributionhelper.attribution.Attribution;
+import com.franmontiel.attributionhelper.attribution.Library;
+import com.franmontiel.attributionhelper.license.LicenseType;
+import com.franmontiel.attributionhelper.sample.util.AttributionHelperCreator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AttributionHelper attributionHelper = AttributionHelperCreator.create(this);
+
+        Button openDialog = (Button) findViewById(R.id.openDialog);
+        openDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = attributionHelper.getDialog();
+                dialog.setTitle(R.string.attributions);
+                dialog.show();
+            }
+        });
+
+        Button openActivity = (Button) findViewById(R.id.openActivity);
+        openActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button openDarkActivity = (Button) findViewById(R.id.openDarkActivity);
+        openDarkActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DarkListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button openCustomActivity = (Button) findViewById(R.id.openCustomActivity);
+        openCustomActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
